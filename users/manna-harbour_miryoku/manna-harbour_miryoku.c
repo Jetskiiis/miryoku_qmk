b6eed51ecb19327fc5cf31e33cdadd27e49b1902
Jetskiiis <76713599+Jetskiiis@users.noreply.github.com>
2022-04-18 08:44:09 -0500

    Add files via upload

---
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -0,0 +1,89 @@
+// Copyright 2019 Manna Harbour
+// https://github.com/manna-harbour/miryoku
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+#include QMK_KEYBOARD_H
+
+#include "manna-harbour_miryoku.h"
+
+//enum custom_keycodes {
+  //  DN_TH = SAFE_RANGE,
+    //KU_QU
+//};
+
+enum combo_events {
+    COMZ,
+    DN_TH,
+    KU_QU,
+};
+
+const uint16_t PROGMEM COMZ_COMBO[] = {KC_J, KC_G, COMBO_END};
+const uint16_t PROGMEM dn_th_STR[] = {KC_D, KC_N, COMBO_END};
+//const uint16_t PROGMEM H_LM_combo[] = {KC_G, KC_M, COMBO_END};
+const uint16_t PROGMEM ku_qu_STR[]    = {KC_U, KC_K, COMBO_END};
+
+combo_t key_combos[] = {
+    [COMZ] =  COMBO_ACTION(COMZ_COMBO),
+    [DN_TH] = COMBO_ACTION(dn_th_STR),
+    [KU_QU] = COMBO_ACTION(ku_qu_STR)
+    };
+
+void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch (combo_index) {
+        case DN_TH:
+            if (pressed) {
+                SEND_STRING("th");
+            }
+            break;
+        case KU_QU:
+            if (pressed) {
+                SEND_STRING("qu");
+            }
+            break;
+    }
+}
+// empty functions cannot exist in QMK
+// bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+// };
+enum layers { MIRYOKU_LAYER_NAMES };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [BASE]   = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_BASE),
+  [NAV]    = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_NAV),
+  [MOUSE]  = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_MOUSE),
+  [MEDIA]  = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_MEDIA),
+  [NUM]    = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_NUM),
+  [SYM]    = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_SYM),
+  [FUN]    = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_FUN),
+  [BUTTON] = U_MACRO_VA_ARGS(LAYOUT_miryoku, MIRYOKU_LAYER_BUTTON)
+};
+
+#if defined (MIRYOKU_KLUDGE_THUMBCOMBOS)
+const uint16_t PROGMEM thumbcombos_base_right[] = {LT(SYM, KC_ENT), LT(NUM, KC_BSPC), COMBO_END};
+const uint16_t PROGMEM thumbcombos_base_left[] = {LT(NAV, KC_SPC), LT(MOUSE, KC_TAB), COMBO_END};
+const uint16_t PROGMEM thumbcombos_nav[] = {KC_ENT, KC_BSPC, COMBO_END};
+const uint16_t PROGMEM thumbcombos_mouse[] = {KC_BTN1, KC_BTN3, COMBO_END};
+const uint16_t PROGMEM thumbcombos_media[] = {KC_MSTP, KC_MPLY, COMBO_END};
+const uint16_t PROGMEM thumbcombos_num[] = {KC_0, KC_MINS, COMBO_END};
+  #if defined (MIRYOKU_LAYERS_FLIP)
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_UNDS, KC_LPRN, COMBO_END};
+  #else
+const uint16_t PROGMEM thumbcombos_sym[] = {KC_RPRN, KC_UNDS, COMBO_END};
+  #endif
+const uint16_t PROGMEM thumbcombos_fun[] = {KC_SPC, KC_TAB, COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+  COMBO(thumbcombos_base_right, LT(FUN, KC_DEL)),
+  COMBO(thumbcombos_base_left, LT(MEDIA, KC_ESC)),
+  COMBO(thumbcombos_nav, KC_DEL),
+  COMBO(thumbcombos_mouse, KC_BTN2),
+  COMBO(thumbcombos_media, KC_MUTE),
+  COMBO(thumbcombos_num, KC_DOT),
+  #if defined (MIRYOKU_LAYERS_FLIP)
+  COMBO(thumbcombos_sym, KC_RPRN),
+  #else
+  COMBO(thumbcombos_sym, KC_LPRN),
+  #endif
+  COMBO(thumbcombos_fun, KC_APP)
+};
+#endif
